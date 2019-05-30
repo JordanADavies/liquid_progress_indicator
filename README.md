acb5b7d60c022d25444cb21fc66a1af1fa4ffee5
Jordan Davies <Jordan.Davies@castletonplc.com>
2019-05-30 15:51:50 +0100

    Updated documentation

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# liquid_progress_indicator
+# liquid_progress_indicator  
+  
+Liquid indicator for Flutter.  
+  
+# Features  
+  
+ - [ ] Liquid linear progress indicator.
+ - [ ] More customisations.
+ - [x] Works similarly to Flutters own ProgressIndicator.
+ - [x] Liquid circular progress indicator.
+  
+## Getting Started  
+  
+Add the following to your `pubspec.yaml`
 
-A new Flutter package.
+    dependencies:  
+      liquid_progress_indicator: ^0.0.1
 
-## Getting Started
-
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+Run `flutter packages upgrade` or update your packages
--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 10),
+    );
+
+    _animationController.addListener(() => setState(() {}));
+    _animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final percentage = _animationController.value * 100;
     return Scaffold(
       body: Center(
         child: Container(
           width: 150.0,
           height: 150.0,
           child: LiquidCircularProgressIndicator(
-            value: 0.5,
+            value: _animationController.value,
             backgroundColor: Colors.white,
             valueColor: AlwaysStoppedAnimation(Colors.blue),
+            center: Text(
+              "${percentage.toStringAsFixed(0)}%",
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
       ),
