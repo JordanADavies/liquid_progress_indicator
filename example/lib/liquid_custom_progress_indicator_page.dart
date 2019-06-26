e30264db3f125751fec82c516884813896701ded
Jordan Davies <Jordan.Davies@castletonplc.com>
2019-06-26 12:35:04 +0100

    Updated examples.

--- example/lib/liquid_custom_progress_indicator_page.dart
+++ example/lib/liquid_custom_progress_indicator_page.dart
@@ -8,20 +8,114 @@ class LiquidCustomProgressIndicatorPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Liquid Custom Progress Indicators"),
       ),
-      body: Center(
-        child: LiquidCustomProgressIndicator(
-          direction: Axis.vertical,
-          shapePath: _buildPath(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _AnimatedLiquidCustomProgressIndicator(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              LiquidCustomProgressIndicator(
+                direction: Axis.vertical,
+                value: 0.2,
+                shapePath: _buildBoatPath(),
+              ),
+              LiquidCustomProgressIndicator(
+                direction: Axis.vertical,
+                backgroundColor: Colors.grey[300],
+                valueColor: AlwaysStoppedAnimation(Colors.red),
+                shapePath: _buildCupPath(),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Path _buildBoatPath() {
+    return Path()
+      ..moveTo(15, 120)
+      ..lineTo(0, 85)
+      ..lineTo(50, 85)
+      ..lineTo(50, 0)
+      ..lineTo(105, 80)
+      ..lineTo(60, 80)
+      ..lineTo(60, 85)
+      ..lineTo(120, 85)
+      ..lineTo(105, 120)
+      ..close();
+  }
+
+  Path _buildCupPath() {
+    return Path()
+      ..lineTo(120, 0)
+      ..lineTo(95, 120)
+      ..lineTo(35, 120)
+      ..close();
+  }
+}
+
+class _AnimatedLiquidCustomProgressIndicator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() =>
+      _AnimatedLiquidCustomProgressIndicatorState();
+}
+
+class _AnimatedLiquidCustomProgressIndicatorState
+    extends State<_AnimatedLiquidCustomProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 10),
+    );
+
+    _animationController.addListener(() => setState(() {}));
+    _animationController.repeat();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final percentage = _animationController.value * 100;
+    return Center(
+      child: LiquidCustomProgressIndicator(
+        value: _animationController.value,
+        direction: Axis.vertical,
+        backgroundColor: Colors.white,
+        valueColor: AlwaysStoppedAnimation(Colors.green),
+        shapePath: _buildHeartPath(),
+        center: Text(
+          "${percentage.toStringAsFixed(0)}%",
+          style: TextStyle(
+            color: Colors.lightGreenAccent,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
   }
 
-  Path _buildPath() {
+  Path _buildHeartPath() {
     return Path()
-      ..lineTo(75, 0)
-      ..lineTo(60, 100)
-      ..lineTo(15, 100)
+      ..moveTo(75, 40)
+      ..cubicTo(75, 37, 70, 25, 50, 25)
+      ..cubicTo(20, 25, 20, 62.5, 20, 62.5)
+      ..cubicTo(20, 80, 40, 102, 75, 120)
+      ..cubicTo(110, 102, 130, 80, 130, 62.5)
+      ..cubicTo(130, 62.5, 130, 25, 100, 25)
+      ..cubicTo(85, 25, 75, 37, 75, 40)
       ..close();
   }
 }
